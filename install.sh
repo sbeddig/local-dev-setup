77fd58511c9d14ad7de8669bde29abda2f15c885
Simon Beddig <simon.beddig@googlemail.com>
2020-10-24 12:57:32 +0200

    update

--- install.sh
+++ install.sh
@@ -30,31 +30,33 @@ install_common_apps() {
 }
 
 configure_zsh() {
-  sudo apt install -y zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
+  if ! command -v zsh; then
+    sudo apt install -y zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
 
-#  cat <<\EOF >>"$HOME"/.bashrc
-#
-#  export SHELL=$(which zsh)
-#  [ -z "$ZSH_VERSION" ] && exec "$SHELL" -l
-#EOF
-  zsh_path=$(command -v zsh)
-  chsh -s "$zsh_path"
+    #  cat <<\EOF >>"$HOME"/.bashrc
+    #
+    #  export SHELL=$(which zsh)
+    #  [ -z "$ZSH_VERSION" ] && exec "$SHELL" -l
+    #EOF
+    zsh_path=$(command -v zsh)
+    chsh -s "$zsh_path"
 
-  curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+    curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
 
-  mkdir -p ~/.zsh/completion
-  curl -L https://raw.githubusercontent.com/docker/compose/1.27.4/contrib/completion/zsh/_docker-compose >~/.zsh/completion/_docker-compose
+    mkdir -p ~/.zsh/completion
+    curl -L https://raw.githubusercontent.com/docker/compose/1.27.4/contrib/completion/zsh/_docker-compose >~/.zsh/completion/_docker-compose
 
-  cp "$INSTALL_DIR"/zsh/.bash_aliases ~/.bash_aliases
-  cp "$INSTALL_DIR"/zsh/.zshrc ~/.zshrc
-  cp "$INSTALL_DIR"/zsh/.zsh_plugins.txt ~/.zsh_plugins.txt
-  cp "$INSTALL_DIR"/zsh/.p10k.zsh ~/.p10k.zsh
+    cp "$INSTALL_DIR"/zsh/.bash_aliases ~/.bash_aliases
+    cp "$INSTALL_DIR"/zsh/.zshrc ~/.zshrc
+    cp "$INSTALL_DIR"/zsh/.zsh_plugins.txt ~/.zsh_plugins.txt
+    cp "$INSTALL_DIR"/zsh/.p10k.zsh ~/.p10k.zsh
+  fi
 }
 
 install_dev_apps() {
-  sudo snap install intellij-idea-ultimate --classic &&
-    sudo ln -s /snap/intellij-idea-ultimate/current/bin/idea.sh /usr/local/bin/idea || true
+  sudo snap install intellij-idea-ultimate --classic
+  sudo ln -s /snap/intellij-idea-ultimate/current/bin/idea.sh /usr/local/bin/idea || true
   sudo snap install code --classic
   sudo snap install drawio
   sudo apt install -y \
@@ -64,16 +66,16 @@ install_dev_apps() {
     docker-compose \
     awscli
 
-  sudo groupadd docker
-  sudo usermod -aG docker "$USER"
+  sudo groupadd docker || true
+  sudo usermod -aG docker "$USER" || true
 }
 
 clone_repos() {
   mkdir -p "$HOME"/repositories
   cd "$HOME"/repositories
-  repositories=$(cat repos)
+  repositories=$(cat "$INSTALL_DIR"/repos)
   for repo in $repositories; do
-    git clone git@github.com:sbeddig/"$repo".git
+    git clone git@github.com:sbeddig/"$repo".git || true
   done
   cd "$INSTALL_DIR"
 }
@@ -87,7 +89,7 @@ configure_desktop() {
   dconf write /org/gnome/desktop/interface/gtk-theme "'Yaru-dark'"
   dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'BOTTOM'"
   dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed false
-  dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'intellij-idea-ultimate_intellij-idea-ultimate.desktop', 'org.gnome.Terminal.desktop']"
+  dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'intellij-idea-ultimate_intellij-idea-ultimate.desktop', 'org.gnome.Terminal.desktop', 'code_code.desktop']"
   dconf write /org/gnome/desktop/interface/clock-show-weekday true
 
   sudo apt install gnome-shell-extension-system-monitor
@@ -103,13 +105,14 @@ configure_desktop() {
   gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
 
   install_shell_extension unitehardpixel.eu.v43.shell-extension.zip
+  dconf write /org/gnome/shell/extensions/unite/window-buttons-placement "'left'"
 }
 
 install_shell_extension() {
   extension=$1
   wget "https://extensions.gnome.org/extension-data/$extension"
   uuid=$(unzip -c "$extension" metadata.json | tail -n+3 | jq -r .uuid)
-  gnome-extensions install "$extension"
+  gnome-extensions install "$extension" --force
   gnome-extensions enable "$uuid"
   rm "$extension"
 }
@@ -126,19 +129,25 @@ set_wallpaper() {
 }
 
 install_dev_libs() {
-  curl -s "https://get.sdkman.io" | bash
-  # shellcheck disable=SC1090
-  source ~/.sdkman/bin/sdkman-init.sh || true
-  sdk install java 8.0.265-amzn
+#  if ! command -v sdk; then
+#    curl -s "https://get.sdkman.io" | bash
+#    # shellcheck disable=SC1090
+#    source ~/.sdkman/bin/sdkman-init.sh || true
+#    sdk install java 8.0.265-amzn
+#  fi'
 
-  curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-  sudo apt install -y nodejs gcc g++ make
-  curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-  sudo apt update && sudo apt install yarn
+  if ! node --version; then
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    sudo apt install -y nodejs gcc g++ make
+    curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt update && sudo apt install yarn
+  fi
 
-  sudo add-apt-repository ppa:longsleep/golang-backports -y
-  sudo apt update && sudo apt install golang-go -y
+  if ! go version; then
+    sudo add-apt-repository ppa:longsleep/golang-backports -y
+    sudo apt update && sudo apt install golang-go -
+  fi
 }
 
 install_npm_libs() {
@@ -152,7 +161,6 @@ update_npm_libs() {
 install_vscode_plugins() {
   code --install-extension amazonwebservices.aws-toolkit-vscode
   code --install-extension ms-azuretools.vscode-docker
-
 }
 
 laptop_tools() {
