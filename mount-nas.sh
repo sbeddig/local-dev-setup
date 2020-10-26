01130e21d38aa88ccbedca97f4c97ddbdcb9b4f2
Simon Beddig <simon.beddig@googlemail.com>
2020-10-26 11:47:15 +0100

    update

--- mount-nas.sh
+++ mount-nas.sh
@@ -11,10 +11,10 @@ mkdir -p /home/simon/Beddig-NAS/video
 
 cat <<EOF | sudo tee -a /etc/fstab
 
-192.168.2.29:/volume1/ebooks /home/simon/Beddig-NAS/ebooks nfs auto,user,defaults,tcp,intr 0 0
-192.168.2.29:/volume1/homes /home/simon/Beddig-NAS/homes nfs auto,user,defaults,tcp,intr 0 0
-192.168.2.29:/volume1/music /home/simon/Beddig-NAS/music nfs auto,user,defaults,tcp,intr 0 0
-192.168.2.29:/volume1/photo /home/simon/Beddig-NAS/photo nfs auto,user,defaults,tcp,intr 0 0
-192.168.2.29:/volume1/programs /home/simon/Beddig-NAS/programs nfs auto,user,defaults,tcp,intr 0 0
-192.168.2.29:/volume1/video /home/simon/Beddig-NAS/video nfs auto,user,defaults,tcp,intr 0 0
+192.168.2.29:/volume1/ebooks /home/simon/Beddig-NAS/ebooks nfs auto,user,defaults,tcp,intr,retry=0 0 0
+192.168.2.29:/volume1/homes /home/simon/Beddig-NAS/homes nfs auto,user,defaults,tcp,intr,retry=0 0 0
+192.168.2.29:/volume1/music /home/simon/Beddig-NAS/music nfs auto,user,defaults,tcp,intr,retry=0 0 0
+192.168.2.29:/volume1/photo /home/simon/Beddig-NAS/photo nfs auto,user,defaults,tcp,intr,retry=0 0 0
+192.168.2.29:/volume1/programs /home/simon/Beddig-NAS/programs nfs auto,user,defaults,tcp,intr,retry=0 0 0
+192.168.2.29:/volume1/video /home/simon/Beddig-NAS/video nfs auto,user,defaults,tcp,intr,retry=0 0 0
 EOF
