9de9bd225f78dbfae62aa40cf91cbc598ecd5e38
Simon Beddig <simon.beddig@googlemail.com>
2020-11-19 11:28:51 +0100

    update

--- install.sh
+++ install.sh
@@ -194,6 +194,13 @@ install_google_chrome() {
   fi
 }
 
+install_albert() {
+  echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+  curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg >/dev/null
+  sudo apt update
+  sudo apt install albert -y
+}
+
 install_common_apps
 configure_zsh
 
