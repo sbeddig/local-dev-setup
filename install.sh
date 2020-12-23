286a347834e215c18fd499d30321c20c5c3b92d0
Simon Beddig <simon.beddig@googlemail.com>
2020-12-23 13:19:54 +0100

    add quicktile

--- install.sh
+++ install.sh
@@ -206,12 +206,20 @@ cleanup() {
   sudo apt autoremove -y
 }
 
+install_quicktile() {
+  sudo apt-get install python3 python3-pip python3-setuptools python3-gi python3-xlib python3-dbus gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-wnck-3.0
+  sudo pip3 install https://github.com/ssokolow/quicktile/archive/master.zip
+  quicktile
+  sed 's/KP_//' "$HOME"/quicktile.cfg
+}
+
 install_common &>/dev/null
 install_zsh &>/dev/null
 install_dev &>/dev/null
 install_npm_libs &>/dev/null
 install_custom_scripts &>/dev/null
 setup_vscode &>/dev/null
+install_quicktile &>/dev/null
 
 configure_desktop &>/dev/null
 set_wallpaper &>/dev/null
