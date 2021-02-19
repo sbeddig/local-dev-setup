e46cc44e24704cf5b232c3f9b5fbf2cfe040f677
Simon Beddig <simon.beddig@googlemail.com>
2021-02-19 22:08:35 +0100

    update

--- custom_scripts/login-aws.sh
+++ custom_scripts/login-aws.sh
@@ -9,10 +9,20 @@ ROLE_ARN=arn:aws:iam::$ACCOUNT_ID:role/custom-admin-role
 ROLE_SESSION=admin-access
 MFA_DEVICE=arn:aws:iam::$ACCOUNT_ID:mfa/mfa_user
 
-aws sts assume-role \
+credentials=$(aws sts assume-role \
   --role-arn "$ROLE_ARN" \
   --role-session-name $ROLE_SESSION \
   --serial-number "$MFA_DEVICE" \
   --token-code "$TOTP" \
   --duration-seconds 3600 \
-  --profile mfa
+  --profile mfa)
+
+aws configure set region eu-west-1 --profile totp
+aws configure set cli_pager "" --profile totp
+aws configure set output json --profile totp
+aws configure set aws_access_key_id "$(echo "$credentials" | jq -r '.Credentials | .AccessKeyId')" --profile totp
+aws configure set aws_secret_access_key "$(echo "$credentials" | jq -r '.Credentials | .SecretAccessKey')" --profile totp
+aws configure set aws_session_token "$(echo "$credentials" | jq -r '.Credentials | .SessionToken')" --profile totp
+
+echo "login to aws successfully."
+
