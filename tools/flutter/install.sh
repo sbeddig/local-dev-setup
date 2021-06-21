ab1f1b5eeadb218a409ab6a1e89e89a16ff600eb
Simon Beddig <simon.beddig@googlemail.com>
2021-06-21 21:01:34 +0200

    wip

---
+++ tools/flutter/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+sudo snap install flutter --classic
+flutter sdk-path
+
+flutter upgrade
+
+wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/4.2.1.0/android-studio-ide-202.7351085-linux.tar.gz
+
+tar -xzf android-studio-ide-202.7351085-linux.tar.gz
+rm android-studio-ide-202.7351085-linux.tar.gz
+
+sudo mv android-studio /opt/android-studio
